36ef4f328c6c20deeaae4a081092fc5f572800fd
aoba0826 <95591188+aoba0826@users.noreply.github.com>
2022-05-17 21:02:37 +0900

    Add ボタン追加

--- sample2/ViewController.swift
+++ sample2/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var label2: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonstart(_ sender: Any) {
+    }
+    
+    @IBAction func buttonstop(_ sender: Any) {
+    }
 }
 
